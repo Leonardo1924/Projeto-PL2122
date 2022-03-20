cfd365e5ade5e471dd5a2daf59b342fe1edfdcd1
emadias <49567783+emadias@users.noreply.github.com>
2022-03-20 13:35:38 +0000

    Update emaFile.py

--- emaFile.py
+++ emaFile.py
@@ -1,4 +1,5 @@
 #import csv
+from fileinput import filename
 import os
 import re
 import ast
@@ -41,27 +42,58 @@ for linha in texto:
              #print(linha[i])
              i=i+1
         l = str(mydic)
-       # print(l)    
+        #print(l)    
         listaparajson.append(l)   
     
+
+if (os.path.isfile('ficheiro.jsp')==True):
+    os.remove('ficheiro.jsp')
+   
 file = open('ficheiro.jsp','w')        
 #print(listaparajson)
 length = len(listaparajson)
 #print(length)
 j=0
+file.write('[')
+file.write('\n')
 while(j<length):
+    
     if(j==0): 
         j+=1
     else: 
-        lst = listaparajson[j]
+        lst = listaparajson[j].split(',')
+      #  lst.append("\n")
         #print(len(lst))
-        #for i in range(len(lst)):
-        # j = int(i)
-            #  lst.insert(j,"\n")
-        print(lst)
-        file.write(lst)
+        i=0
+        while i < len(lst):
+            file.write("\t")
+            pattern = '^{'
+            result = re.match(pattern, lst[i])
+            pattern2 = '}$'
+            result2 = re.search(pattern2, lst[i])
+            if result:
+                lst[i].split('{')
+                file.write('{')
+                file.write('\n\t')
+                file.write(lst[i][1:])
+                file.write('\n')
+            elif result2: 
+                  print("entrou")
+                  lst[i].split('}')
+                  file.write(lst[i][0:-1])  
+                  file.write('\n\t')
+                  file.write('},')
+            else:
+                 file.write(lst[i])
+                 file.write("\n")
+            i=i+1
+           
+      #  print(lst)
+      #  file.write(lst)
+        file.write("\n")
         j+=1
-  
+file.write('\n')
+file.write(']')
 #s = pickle.loads(mydic)
 #s = json.dumps(mydic)
 #print(listaparajson)

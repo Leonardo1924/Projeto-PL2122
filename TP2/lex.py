b9fd5fd6b003f87188a545233c8437200c4d2c12
Ema Dias <49567783+emadias@users.noreply.github.com>
2022-05-09 17:46:52 +0100

    Update lex.py

--- TP2/lex.py
+++ TP2/lex.py
@@ -3,6 +3,7 @@ import re
 import collections
 import tokenize
 
+
 tokens = [ 
           'REGEX',
           'REGEX2',
@@ -12,18 +13,30 @@ tokens = [
           'SIMBNAOTERMINAIS',
           'EPSILON',
           'ARROW',
-          'NEWLINE'
+          'NEWLINE',
+          'OU',
+          'END'
 ]
 
 literals = ["|"]
 
 def t_NEWLINE(t):
     r' \\n'
+    parser.index = parser.index + 1
+    print(parser.index)
     return t 
-  
+
+def t_END(t):
+    r'\\endOU'
+    return t 
+
+def t_OU(t):
+    r'\\and'
+
+    return t
+
 def t_REGEX(t):
     r't_\w+' 
-    print("entrou1")
     return t
     
 def t_REGEX2(t):
@@ -40,7 +53,6 @@ def t_GRAM(t):
 
 def t_SIMBTERMINAIS(t):
     r'[a-z]\w* '
-    print("entrou")
     return t
 
 def t_SIMBNAOTERMINAIS(t):
@@ -55,7 +67,7 @@ def t_ARROW(t):
     r'\-\>'
     return t
 
-t_ignore = ' \n\t'
+t_ignore = ' \t'
 
 def t_error(t):
 	print("Illegal Character: " + t.value[0])
@@ -67,7 +79,14 @@ import ply.yacc as yacc
 
 
 def p_gramatica(p):
-    "gramatica : REGEX REGEX2 REGEX3 GRAM listaProducoes"
+    "gramatica : regexs GRAM listaProducoes"
+    pass
+
+def p_regexs(p):
+    "regexs : regexs REGEX REGEX2 REGEX3"
+    pass
+def p_regex_empty(p):
+    "regexs :"
     pass
 
 def p_gramatica_empty(p):
@@ -75,7 +94,7 @@ def p_gramatica_empty(p):
     pass
 
 def p_listaProducoes_recursivo(p):
-    "listaProducoes : listaProducoes NEWLINE producao"
+    "listaProducoes : listaProducoes NEWLINE producao NEWLINE"
     pass
 
 def p_listaProducoes_elemento(p):
@@ -85,42 +104,72 @@ def p_listaProducoes_elemento(p):
 
 def p_listaProducoes_newline(p):
     "listaProducoes : listaProducoes newline"
+    print("antes do clear", parser.dicionarioFirstFollow)
+    parser.dicionarioLista = []
+    print("depois do clear", parser.dicionarioFirstFollow)
     pass
 
 def p_producao(p):
     "producao : ladoEsq ARROW ladoDir"
     pass
 
+def p_ladoEsq (p):
+    "ladoEsq : SIMBNAOTERMINAIS"
+   
+    parser.key = p[1]
+    parser.keyOU = p[1]
+    print("KEYYYYYY", parser.key)
+    key = ''
+    keyOU = ''
+    pass
+
 def p_newline(p):
-    "newline : NEWLINE producaoSimples"
+    "newline : OU producaoSimples"
     pass
     
 def p_producaoSimples(p): 
-    "producaoSimples : '|' SIMBTERMINAIS"
-    print(p[2])
+    "producaoSimples : '|' SIMBTERMINAIS END"
+    parser.dicionarioOU.append(p[2])
+    print("key",parser.keyOU, "value", parser.dicionarioOU, "key", parser.key, "value", parser.dicionarioLista)
+    parser.dicionarioFirstFollow[parser.keyOU+"_ProducaoOU"] = parser.dicionarioOU 
+    print("adicionou")
+    print(parser.dicionarioFirstFollow)
+    
     pass
                                   
-def p_ladoEsq (p):
-    "ladoEsq : SIMBNAOTERMINAIS"
-    pass
+
     
 def p_ladoDir_simbter(p):
     "ladoDir : SIMBTERMINAIS"
+    print("antes", parser.dicionarioFirstFollow, "KEY", parser.key)
+    parser.dicionarioLista.append(p[1])
+    print("key",parser.key, "value", parser.dicionarioLista)
+    parser.dicionarioFirstFollow[parser.key] = parser.dicionarioLista
+    print(parser.dicionarioFirstFollow)
+    print("adicionou")
     print(p[1])
     pass
 
 def p_ladoDir_epsilon(p):
     "ladoDir : EPSILON "
+    parser.dicionarioLista.append(p[1])
     pass
 
 def p_ladoDir_rec(p):
     "ladoDir : recursividade SIMBTERMINAIS"
     #p.parser.listaTokens.add(p[2])
-    print(p[2])
+    parser.dicionarioLista.append(p[2])
+    print("key",parser.key, "value", parser.dicionarioLista)
+    parser.dicionarioFirstFollow[parser.key] = parser.dicionarioLista
+    print("adicionou")
+    print(parser.dicionarioFirstFollow)
     pass
                        
 def p_recursividade(p):
     "recursividade : SIMBNAOTERMINAIS"
+    parser.dicionarioLista.append(p[1])
+    print("key",parser.key, "value", parser.dicionarioLista)
+    print("Adicionou")
     pass 
    
  
@@ -132,28 +181,37 @@ def p_error(p):
 parser = yacc.yacc()
 parser.listaTokens=set()
 parser.error = False
-
-
+parser.dicionarioFirstFollow = {}
+parser.index = 0
+parser.dicionarioLista = []
+parser.key = ''
+parser.keyOU = ''
+parser.dicionarioOU = []
 tokens_regex = {}
 
 
 arq = open("C:\\Users\\edi8b\\OneDrive\\Ambiente de Trabalho\\texto2.txt","r+")
 f = arq.readlines()
+i=0
 for linha in f:
+    print(parser.index)
     parser.parse(linha)
-    print(lexer.token())
-    print(linha)
     regex = re.search('(t_)(\w+) :: (r\')(.+)(\')',linha) 
     if regex: 
         tokens_regex[regex.group(2)] = regex.group(4)
+    parser.index = parser.index + 1
+    print(parser.dicionarioFirstFollow)
     if (parser.error== True) :
               print("erro na gramatica construida ")
     else :
         print("sucesso")
+    
+ 
 
 try: 
     print(tokens_regex) 
 except KeyError: 
     print('key not present') 
- 
+    
+
 
